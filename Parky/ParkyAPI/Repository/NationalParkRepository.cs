d5016c599f0f98cd304e299a6a2ab33b9d84c2a6
Rampranav <vrampranav9@gmail.com>
2020-11-14 18:01:29 +0530

    Added NationalParkRepository

---
+++ Parky/ParkyAPI/Repository/NationalParkRepository.cs
@@ -0,0 +1,46 @@
+﻿using ParkyAPI.Models;
+using ParkyAPI.Repository.IRepository;
+using System.Collections.Generic;
+
+namespace ParkyAPI.Repository
+{
+    public class NationalParkRepository : INationalParkRepository
+    {
+        public bool CreateNationalPark(NationalPark nationalPark)
+        {
+            throw new System.NotImplementedException();
+        }
+        public bool DeleteNationalPark(NationalPark nationalPark)
+        {
+            throw new System.NotImplementedException();
+        }
+        public NationalPark GetNationalPark(int nationalParkId)
+        {
+            throw new System.NotImplementedException();
+        }
+        public ICollection<NationalPark> GetNationalParks()
+        {
+            throw new System.NotImplementedException();
+        }
+        public bool NationalParkExists(int nationalParkId)
+        {
+            throw new System.NotImplementedException();
+        }
+        public bool NationalParkExists(string nationalParkName)
+        {
+            throw new System.NotImplementedException();
+        }
+        public bool NationalParkExists(NationalPark nationalPark)
+        {
+            throw new System.NotImplementedException();
+        }
+        public bool Save()
+        {
+            throw new System.NotImplementedException();
+        }
+        public bool UpdateNationalPark(NationalPark nationalPark)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
